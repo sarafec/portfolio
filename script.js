f11384b6a6dff1875fa28eff65d681a585a6dc25
sarafec <fecadusa@gmail.com>
2017-10-17 22:03:13 -0700

    modify comments

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 
 (function() {
-	//definitions for modal window
+	// variables for modal window
 	let modal = document.querySelector(".modal");
 	let modalOverlay = document.querySelector(".modal-overlay");
 	let closeButton = document.querySelector(".close-button");
 	let bottomButton = document.querySelector(".bottom-btn");
 	let openList = document.querySelector(".side-project-annotated");
 	
-	//definitions for photo viewer
+	// variables for project viewer
 	let chevronRight = document.querySelector(".chevron-right");
 	let chevronLeft = document.querySelector(".chevron-left");
 	let photoViewerContainer = document.querySelector(".main-project-viewer");
 	let totalCount = document.querySelector(".main-project-icons").children.length;
 	let currentCount = 1;
 
-	//annotated project list events
+	// modal window events
 	closeButton.addEventListener("click", toggleModal);
 	bottomButton.addEventListener("click", toggleModal);
 
@@ -34,7 +34,7 @@
 		}
 	});
 
-	//click or tab project viewer events
+	// project viewer events
 	chevronRight.addEventListener("click", movePhotosRight);
 	chevronRight.addEventListener("keydown", function(evt){
 		if(evt.which === 13){
@@ -49,23 +49,23 @@
 		}
 	});
 
-	//swipe project viewer events
+	// project viewer events - swipe
 	photoViewerContainer.addEventListener("touchstart", handleTouch);
 	photoViewerContainer.addEventListener("touchend", handleTouch);
 	photoViewerContainer.addEventListener("touchcancel", handleTouch);
 
-	//swipe functionality
+	// swipe functionality
 	let touch1 = {x: 0, y: 0, time: 0 };
 	function handleTouch(evt){
-		var touch = evt.touches[0] || evt.changedTouches[0];
+		let touch = evt.touches[0] || evt.changedTouches[0];
 		if (evt.type === 'touchstart'){
 			touch1.x = touch.pageX;
 			touch1.y = touch.pageY;
 			touch1.time = evt.timeStamp;
 		} else if (evt.type === 'touchend'){
-			var dx = touch.pageX - touch1.x;
-			var dy = touch.pageY - touch1.y;
-			var dt = evt.timeStamp - touch1.time;
+			let dx = touch.pageX - touch1.x;
+			let dy = touch.pageY - touch1.y;
+			let dt = evt.timeStamp - touch1.time;
 		
 		if (dt < 500){
 			if (Math.abs(dx) > 60){
@@ -79,14 +79,14 @@
 		}
 	}
 
-	//toggle open or close annotated project list modal
+	// toggle modal window
 	function toggleModal() {
 		modal.classList.toggle("closed");
 		modalOverlay.classList.toggle("closed");
 	}
 
 
-	//move project icons within the project viewer
+	// project viewer - move left
 	function movePhotosLeft() {
 		if (currentCount > 1) {
 			photoViewerContainer.style.transform += "translateX(370px)";
@@ -94,6 +94,7 @@
 		}
 	}
 
+	// project viewer - move right
 	function movePhotosRight() {
 		if(currentCount < totalCount){
 			photoViewerContainer.style.transform += "translateX(-370px)";
@@ -103,4 +104,3 @@
 })();
 
 //fix modal window focus issue
-//add swipe functionality for mobile
